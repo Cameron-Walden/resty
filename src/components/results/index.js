0c22c539dd0b80f2b4b2c4fee2932b97f8c24c58
Cameron <cameronkwalden@gmail.com>
2021-11-28 17:30:31 -0800

    adds id for testing

--- src/components/results/index.js
+++ src/components/results/index.js
@@ -16,7 +16,7 @@ const Results = (props) => {
   return (
   <section>
     {/* <pre>{props.data ? JSON.stringify(props.data, undefined, 2) : null}</pre> */}
-    <pre>{props.data ? JSON.stringify(props.data, undefined, 2) : <p>Loading...</p>}</pre>
+    <pre data-testid="data">{props.data ? JSON.stringify(props.data, undefined, 2) : <p>Loading...</p>}</pre>
     </section>
   );
 }
