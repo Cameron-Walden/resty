b5f5c0859227a2513f3f19ee483d46602dfba567
Cameron <cameronkwalden@gmail.com>
2021-11-28 17:44:26 -0800

    changes back to class component to render data

--- src/App.js
+++ src/App.js
@@ -1,15 +1,50 @@
 import React from 'react';
-import { useState } from 'react';
-
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.scss';
+
 import Header from './components/header';
 import Footer from './components/footer';
 import Form from './components/form';
 import Results from './components/results';
 
-// class App extends React.Component {
+  class App extends React.Component {
 
-  const App = () => {
+    constructor(props) {
+      super(props);
+      this.state = {
+        data: null,
+        requestParams: {},
+      };
+    }
+    
+    callApi = async (requestParams) => {
+      const apiUrl = requestParams.url;
+      const response = await axios.get(apiUrl);
+  
+      const data = {
+        Headers: response.headers,
+        count: response.data.count,
+        Response: response.data.results
+      };
+      this.setState({data, requestParams});
+    }
+  
+    render() {
+      return (
+        <React.Fragment>
+          <Header />
+          <Form handleApiCall={this.callApi}/>
+          <div id='requestMethod'>Request Method: {this.state.requestParams.method}</div>
+          <div id='url'>URL: {this.state.requestParams.url}</div>
+          <Results data={this.state.data} />
+          <Footer />
+        </React.Fragment>
+      );
+    }
+  }
+
+  // const App = () => {
 
   // constructor(props) {
   //   super(props);
@@ -17,35 +52,37 @@ import Results from './components/results';
     //   data: null,
     //   requestParams: {},
     // };
-    const [data, setData] = useState({});
-    const [requestParams, setRequestParams] = useState({});
+    // const [data, setData] = useState(null);
+    // const [requestParams, setRequestParams] = useState({});
+
   
+  // const callApi = (requestParams, requestBody) => {
+  //   const apiData = {
+  //     count: 2,
+  //     results: [
+  //       {name: 'fake thing 1', url: 'http://fakethings.com/1'},
+  //       {name: 'fake thing 2', url: 'http://fakethings.com/2'},
+  //     ],
+  //   };
+  //   // this.setState({data, requestParams});
+  //   setData(apiData);
+  //   setRequestParams(requestParams); 
+  // }
 
-  const callApi = (requestParams) => {
-    const apiData = {
-      count: 2,
-      results: [
-        {name: 'fake thing 1', url: 'http://fakethings.com/1'},
-        {name: 'fake thing 2', url: 'http://fakethings.com/2'},
-      ],
-    };
-    // this.setState({data, requestParams});
-    setData(apiData);
-    setRequestParams(requestParams); 
-  }
 
-    return (
-      <React.Fragment>
-        <Header />
-        {/* <div>Request Method: {this.state.requestParams.method}</div> */}
-        <div>Request Method: {requestParams.method}</div>
-        {/* <div>URL: {this.state.requestParams.url}</div> */}
-        <div>URL: {requestParams.url}</div>
-        <Form handleApiCall={callApi} />
-        <Results data={data} />
-        <Footer />
-      </React.Fragment>
-    );
-  }
+  //   return (
+  //     <React.Fragment>
+  //       <Header />
+  //       {/* <div>Request Method: {this.state.requestParams.method}</div> */}
+  //       <div>Request Method: {requestParams.method}</div>
+  //       {/* <div>URL: {this.state.requestParams.url}</div> */}
+  //       <div>URL: {requestParams.url}</div>
+  //       <Form handleApiCall={callApi} />
+  //       <Results data={data} />
+  //       <Footer />
+  //     </React.Fragment>
+  //   );
+  // }
 
+  
 export default App;
