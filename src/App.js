4a79626e96405459363f386768aa36ec427e4e13
Cameron <cameronkwalden@gmail.com>
2021-11-29 20:28:10 -0800

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,52 +1,53 @@
-import React from 'react';
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import './App.scss';
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.scss";
 
-import Header from './components/header';
-import Footer from './components/footer';
-import Form from './components/form';
-import Results from './components/results';
+import Header from "./components/header";
+import Footer from "./components/footer";
+import Form from "./components/form";
+import Results from "./components/results";
 
-  function App() {
+const App = () => {
+  const [data, setData] = useState(null);
+  const [requestParams, setRequestParams] = useState({});
+  const [results, setResults] = useState({});
 
-    const [data, setData] = useState(null);
-    const [requestParams, setRequestParams] = useState({})
+  useEffect(() => {
+    const apiUrl = requestParams.url;
+    axios
+      .get(apiUrl)
+      .then((response) => {
+        setResults(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  });
 
-    // useEffect(() => {
-    //   const apiUrl = requestParams.url;
-    //   axios.get(apiUrl)
-    //   .then(response => {
-    //     setResults(response.data)
-    //   }).catch(error => {
-    //     console.log(error);
-    //   })
-    // }, []);
-    
-    const callApi = async (apiParams) => {
-      const apiUrl = apiParams.url;
-      const response = await axios.get(apiUrl);
-  
-      const data = {
-        Headers: response.headers,
-        count: response.data.count,
-        Response: response.data.results
-      };
-      setData(data);
-      setRequestParams({...requestParams, ...apiParams})
-    }
+  const callApi = async (apiParams) => {
+    const apiUrl = apiParams.url;
+    const response = await axios.get(apiUrl);
 
-      return (
-        <React.Fragment>
-          <Header />
-          <Form handleApiCall={callApi}/>
-          <div id='requestMethod'>Request Method: {requestParams.method}</div>
-          <div id='url'>URL: {requestParams.url}</div>
-          <Results data={data} />
-          <Footer />
-        </React.Fragment>
-      );
-    }
+    const data = {
+      headers: response.headers,
+      count: response.data.count,
+      response: response.data.results,
+    };
+    setData(data);
+    setRequestParams({ ...requestParams, ...apiParams });
+  };
 
+  return (
+    <React.Fragment>
+      <Header />
+      <Form handleApiCall={callApi} />
+      <div id="requestMethod">Request Method: {requestParams.method}</div>
+      <div id="url">URL: {requestParams.url}</div>
+      <Results data={data} />
+      <Footer />
+    </React.Fragment>
+  );
+};
 
 export default App;
