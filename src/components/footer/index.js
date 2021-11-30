da98db4964f648a05a7ccb919d0acb523fd44b02
Cameron <cameronkwalden@gmail.com>
2021-11-29 20:28:28 -0800

    adds name

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -3,9 +3,8 @@ import './footer.scss';
 
 class Footer extends React.Component {
   render() {
-    return <footer>&copy; 2021</footer>;
+    return <footer>Cameron Walden &copy; 2021</footer>;
   }
 }
-
 
 export default Footer;
